82cebfe778f2d64bd13846f699989ee9d21cac0b
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2021-11-19 08:15:59 +0530

    wallet

---
+++ contracts/Wallet.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Wallet {
+    address payable public owner;
+
+    constructor(address payable _owner) {
+        owner = _owner;
+    }
+
+    function deposit() public payable {}
+
+    function sendEther(address payable to, uint256 amount) public {
+        if (msg.sender == owner) {
+            to.transfer(amount);
+            return;
+        }
+        revert("sender is not allowed");
+    }
+
+    function balanceOf() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
