4d5b456096e8d91841b40167ebc63b52b4791474
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2021-11-19 10:57:56 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  networks: {  
-  
-            },
+  networks: {},
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
@@ -15,5 +13,17 @@ module.exports = {
   },
 
   // Configure your compilers
-  compilers: {},
+  compilers: {
+    solc: {
+      version: "0.8.7", // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
 };
